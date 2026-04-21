874e6b39f31e80cbc4686b86585e37a70cc29118
diak <diak@mesterlevel.lo>
2026-04-21 10:39:27 +0200

    jo bcrypt

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = require('./app')
 const PORT = config.PORT
 const HOST = 'localhost'
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
     console.log(`Szerver IP: http://${HOST}:${PORT}`)
 })
