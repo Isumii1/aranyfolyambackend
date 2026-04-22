3ba9510c9df9e3d1aa434dd48f50413f6a825d65
Attila428 <pelei.attila.mark.404@dszcbaross.edu.hu>
2026-04-22 13:28:36 +0200

    cors changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const categoryRoutes = require('./routes/categoryRoutes')
 const orderRoutes = require('./routes/orderRoutes')
 
 app.use(cors({
-    origin: ['http://127.0.0.1:5173', 'http://localhost:5173','*'],
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173','https://aranyfolyam.netlify.app'],
     credentials:true
 }))
 app.use(express.json())
